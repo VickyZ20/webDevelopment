47a156b3c29a6d0d3a4774e3c656e21c9cae5238
VickyZ20 <59904377+VickyZ20@users.noreply.github.com>
2020-10-04 17:58:14 -0700

    Delete main.js

--- webPage/main.js
+++
@@ -1,9 +0,0 @@
-function downfn() {
-  console.log("down:" + event.keyCode);
-  if (event.keyCode == 13) {
-    i1.onclick();
-  }
-}
-function upfn() {
-  console.log("up：" + String.fromCharCode(event.keyCode));
-}
